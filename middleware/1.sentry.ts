ab2191ff969069ae74eef32761881b18050d5de0
FuBaooo <baihe2233@gmail.com>
2024-05-08 13:24:36 +0800

    Fix memory leak in Sentry middleware

--- middleware/1.sentry.ts
+++ middleware/1.sentry.ts
@@ -3,4 +3,11 @@ import * as Sentry from '@sentry/node'
 export default fromNodeMiddleware((req, res, next) => {
   const sentryTracingMiddleware = Sentry.Handlers.tracingHandler()
   sentryTracingMiddleware(req, res, next)
+
+  // handle memory leak
+  res.on('finish', () => {
+    setImmediate(() => {
+      Sentry.getCurrentScope()?.clear()
+    })
+  })
 })
